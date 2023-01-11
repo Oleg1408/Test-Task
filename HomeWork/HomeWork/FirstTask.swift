1ea9da120b4929139f768eeaded9c295001fa833
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-01-11 23:12:26 +0200

    new value

--- HomeWork/HomeWork/FirstTask.swift
+++ HomeWork/HomeWork/FirstTask.swift
@@ -9,19 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Array
     let arrayOfNumbers = [-1, -2, -3, -4, -5, 1, 2, 3, 4, 5]
     let sumOfNumber = [2, 3, 10, 20, 5]
+    
+    // Dictionary
+    let arrayOfString = ["january", "fabruary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
+    
+    var calendar: [Int:[String]] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // arrayOfNumbers
         print(positiveNumber(number: arrayOfNumbers))
         print(maxAndMinElement(number: arrayOfNumbers))
         print(sumOfAllNumbers(numbers: sumOfNumber))
         
+        // arrayOfString
+        
+        print(arrayOfDic(mounth: arrayOfString))
+        print(dic(value: dictionary))
+        
+    
     }
     
     // MARK: - Array
+    
+    /*
+    Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля
+    Замените все отрицательные числа на 0 и выведите массив в консоль
+    Найдите минимальный и максимальный элемент в массиве и выведите его в консоль
+    Найдите сумму элементов массива чисел и выведите ее в консоль
+     */
     
     func positiveNumber (number: [Int]) -> [Int] {
         
@@ -61,9 +82,55 @@ class ViewController: UIViewController {
         return sum
     }
     
-    // Stop Array
     
+    // MARK: - Dictionary
     
+    /*
+     Создайте массив с названиями всех месяцев, типа String. Затем создайте словарь и с
+     помощью цикла задайте ему значения на основании массива: где в качестве ключей
+     будут выступать цифры (индексы), а в качестве значений - элементы массива.
+     Создайте словарь:
+     var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
+     Поменяйте местами значения по ключам “first” и “fourth”. Выведите в консоль итоговый
+     словарь
+     
+     */
+    
+    func arrayOfDic(mounth: [String]) -> [Int:String] {
+        var dic: [Int:String] = [:]
+        
+        for (index, mounth) in arrayOfString.enumerated() {
+            let _ = dic.updateValue(mounth, forKey: index + 1)
+        }
+        return dic
+    }
+    
+    //
+    
+    func dic(value: [String:Int]) -> [String:Int] {
+        var newDic: [String:Int] = [:]
+
+        let _ = newDic.updateValue(1, forKey: "fourth")
+        let _ = newDic.updateValue(4, forKey: "first")
+        
+        return newDic
+    }
+    
+    /*
+     Создайте пустой словарь calendar, так же вам понадобиться массив с месяцами из
+     четвертого задания.
+     С помощью цикла от 1970 до 2022 задайте значения для словаря. В качестве ключа
+     пусть будет год, а значение это массив с месяцами.
+     С помощью другого цикла добавьте в calendar на каждый год по одному месяцу с
+     вашим названием, чтобы в каждом году вышло 13 месяцев.
+     */
+    
+    func value() {
+    
+        for i in 1970...2022 {
+            print(i)
+        }
+    }
     
     
 }
