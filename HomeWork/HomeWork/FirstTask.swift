64b7e6e4e587a8aa7c9dac02d93d3735f2eb2228
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-01-24 22:20:49 +0200

    end commit

--- HomeWork/HomeWork/FirstTask.swift
+++ HomeWork/HomeWork/FirstTask.swift
@@ -21,36 +21,36 @@ class ViewController: UIViewController {
     var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
     //
     var calendar: [Int:[String]] = [:]
-    
-    let phraseUsa = "Hello, how are you!"
+    //
+    let phraseUsa = "Hello how are you!"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        // 1
-//        print(positiveNumber(number: arrayOfNumbers))
-//        // 2
-//        print(maxAndMinElement(number: arrayOfNumbers))
-//        // 3
-//        print(sumOfAllNumbers(numbers: sumOfNumber))
-//        // 4
-//        print(arrayOfDic(mounth: arrayOfMounth))
-//        // 5
-//        print(dic(value: dictionary))
-//        // 6
-//        print(yearCalendar(mounth: arrayOfMounth))
-//        // 7
-//        number(numberOne: 6, numberTwo: 1)
-//        // 8
-//        print(createCalendarPro())
-//        // 9
-//        print(mobile(percent: 34))
-//        // 10
-        print(createPhrase(phrase: phraseUsa))
-
+        
+        // 1
+        print(positiveNumber(number: arrayOfNumbers))
+        // 2
+        print(maxAndMinElement(number: arrayOfNumbers))
+        // 3
+        print(sumOfAllNumbers(numbers: sumOfNumber))
+        // 4
+        print(arrayOfDic(mounth: arrayOfMounth))
+        // 5
+        print(dic(value: dictionary))
+        // 6
+        print(yearCalendar(mounth: arrayOfMounth))
+        // 7
+        number(numberOne: 6, numberTwo: 1)
+        // 8
+        print(createCalendarPro())
+        // 9
+        print(mobile(percent: 34))
+        // 10
+        createPhrase(phrase: phraseUsa)
+        
     }
-
+    
     /*
      Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля
      Замените все отрицательные числа на 0 и выведите массив в консоль
@@ -153,7 +153,6 @@ class ViewController: UIViewController {
      ● Создайте новый словарь calendarPro, где в качестве ключей будут года, в качестве
      значений вложенные словари. В которых в качестве ключей названия месяцев, а в
      качестве значений массив с днями созданный выше.
-     ● Выведите в консоль 1 сентября 2019 года.
      PS: Для заполнения calendarPro используйте только созданный массив с числами, и
      словарь с месяцами, и никаких других данных
      */
@@ -163,12 +162,12 @@ class ViewController: UIViewController {
             day.append(item)
         }
         let calendarPro = [year: [arrayOfMounth: day]]
+        
         for (years, mounth) in calendarPro {
-            print("\(years) - \(mounth)")
+            print("\(mounth.values) - \(mounth.keys) - \(years)")
         }
     }
-    
-    
+        
     /*
      Создайте 2 переменные с числами. Если значения равны, выведите в консоль их
      сумму умноженную на 3, если нет, выведите в консоль их сумму в случае если она четное
@@ -180,7 +179,6 @@ class ViewController: UIViewController {
      В этом задании необходимо найти способ доставать слова из вашей строки, погуглите
      методы с помощью которых это возможно реализовать.
      */
-    
     
     func number(numberOne: Int, numberTwo: Int) {
         var sum = 0
@@ -196,18 +194,16 @@ class ViewController: UIViewController {
         }
     }
     
-    
     func createPhrase(phrase: String) -> String {
         let newPhrase = phrase
-        let index = newPhrase.firstIndex(of: ",") ?? newPhrase.endIndex
-        let beginning = newPhrase[..<index]
-        print(beginning)
-
+        
+        let firstWord = newPhrase[newPhrase.index(newPhrase.startIndex, offsetBy: 0)...newPhrase.index(newPhrase.startIndex, offsetBy: 4)]
+        print(firstWord)
+        let secondWord = newPhrase[newPhrase.index(newPhrase.endIndex, offsetBy: -4)...newPhrase.index(newPhrase.endIndex, offsetBy: -2)]
+        print(secondWord)
         
         return newPhrase
     }
-    
-    
     
     /*
      Создайте функцию которая принимает 1 параметр, процент заряда батареи. Пусть
@@ -218,7 +214,6 @@ class ViewController: UIViewController {
      ● если 0%, “Устройство полностью разряжено”
      В остальных случаях не выводите ничего. Используйте switch case
      */
-    
     
     func mobile(percent: Int) -> String {
         let percent = percent
