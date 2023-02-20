33dc55de589466e85b788208b2d76146242ac159
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-02-20 22:57:05 +0200

    Task Two (4)

--- HomeWork/HomeWork/SecondTask.swift
+++ HomeWork/HomeWork/SecondTask.swift
@@ -7,20 +7,48 @@
 
 import UIKit
 
-
 class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 1
+        //        let alexSailor = Sailor(name: "Alex")
+        //        let momoSailor = Sailor(name: "Momo")
+        //        momoSailor.introduceMyself()
+        //        alexSailor.introduceMyself()
+        
+        // 2
+        //        let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: ["Alex", "Momo"])
+        //        ship.introduceAll()
+        
+        // 3
+        //
+        //        let alexSailor = Sailor(name: "Alex")
+        //        let momoSailor = Sailor(name: "Momo")
+        //        let benSailor = Sailor(name: "Ben")
+        //        let bodSailor = Sailor(name: "Bob")
+        //        let jacknSailor = Sailor(name: "Jack")
+        //
+        //        let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
+        //        ship.introduceAll()
+        
+        
+        // 4
+        
         let alexSailor = Sailor(name: "Alex")
         let momoSailor = Sailor(name: "Momo")
-        momoSailor.introduceMyself()
-        alexSailor.introduceMyself()
+        let benSailor = Sailor(name: "Ben")
+        let bodSailor = Sailor(name: "Bob")
+        let jacknSailor = Sailor(name: "Jack")
         
         
-        let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: ["Alex", "Momo"])
-        ship.introduceAll()
+        let titanicSailor = TitanicSeilor(name: "Bob")
+        titanicSailor.introduceMyself()
+        
+        let titanic = Titanic(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
+        titanic.introduceAll()
+        
         
     }
 }
@@ -35,7 +63,7 @@ class Sailor {
     var name: String
     
     func introduceMyself() {
-        print( "Hello, my name is \(name)")
+        print( "Привет, меня зовут \(name)")
     }
     
     init(name: String) {
@@ -49,18 +77,45 @@ class Sailor {
 //  корабля *имя корабля*"
 
 
+// 3*. Через инициализатор создайте 5 экземпляров класса Матрос и класс Корабль, в
+// который передайте список созданных матросов. Вызовите функцию introduceAll. Вы
+// должны получить в консоли приветствие от имени всех матросов и название корабля
+
 class Ship {
     
     var nameOfShip: String
-    var nameOfSailors =  [String]()
+    var nameOfSailors =  [Any]()
     
     func introduceAll() {
-        print("Hello from \(nameOfSailors). We are from \(nameOfShip).")
+        print("Привет от \(nameOfSailors). Мы с \(nameOfShip).")
     }
     
-    init(nameOfShip: String, nameOfSailors: [String]) {
+    init(nameOfShip: String, nameOfSailors: [Any]) {
         self.nameOfShip = nameOfShip
         self.nameOfSailors = nameOfSailors
+        
+    }
+}
+
+
+//  4. Создайте класс Titanic, наследник класса Ship. И класс TitanicSeilor, наследник
+//  Seilor. Пусть TitanicSeilor будет иметь ту же функцию introduseMyself, но сообщение будет
+//  выводить на английском. Аналогично класс Titanic, только пусть его функция introduceAll
+//  выводит в на двух языках: то же сообщение что и Ship, а после - такое же сообщение на
+//  английском. Не меняйте существующие классы Ship и Seilor
+
+class Titanic: Ship {
+    
+    override func introduceAll() {
+        super.introduceAll()
+        print("Hello from \(nameOfSailors). We are from \(nameOfShip)")
+    }
+}
+
+class TitanicSeilor: Sailor {
+    
+    override func introduceMyself() {
+        print("Hello, my name is \(name)")
     }
     
 }
