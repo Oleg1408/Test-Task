2a909d1275c6fb1b523830c28570ba629d2704ab
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-03-13 22:20:07 +0200

    new commit

--- HomeWork/HomeWork/SecondTask.swift
+++ HomeWork/HomeWork/SecondTask.swift
@@ -8,53 +8,90 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 1
-//                let alexSailor = Sailor(name: "Alex")
-//                let momoSailor = Sailor(name: "Momo")
-//                momoSailor.introduceMyself()
-//                alexSailor.introduceMyself()
+        //                let alexSailor = Sailor(name: "Alex")
+        //                let momoSailor = Sailor(name: "Momo")
+        //                momoSailor.introduceMyself()
+        //                alexSailor.introduceMyself()
         // 2
-//                let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: ["Alex", "Momo"])
-//                ship.introduceAll()
+        //                let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: ["Alex", "Momo"])
+        //                ship.introduceAll()
         // 3
-//                let alexSailor = Sailor(name: "Alex")
-//                let momoSailor = Sailor(name: "Momo")
-//                let benSailor = Sailor(name: "Ben")
-//                let bodSailor = Sailor(name: "Bob")
-//                let jacknSailor = Sailor(name: "Jack")
-//
-//                let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
-//                ship.introduceAll()
+        //                let alexSailor = Sailor(name: "Alex")
+        //                let momoSailor = Sailor(name: "Momo")
+        //                let benSailor = Sailor(name: "Ben")
+        //                let bodSailor = Sailor(name: "Bob")
+        //                let jacknSailor = Sailor(name: "Jack")
+        //
+        //                let ship = Ship(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
+        //                ship.introduceAll()
         // 4
         
-//                let alexSailor = Sailor(name: "Alex")
-//                let momoSailor = Sailor(name: "Momo")
-//                let benSailor = Sailor(name: "Ben")
-//                let bodSailor = Sailor(name: "Bob")
-//                let jacknSailor = Sailor(name: "Jack")
-//
-//
-//                let titanicSailor = TitanicSeilor(name: "Bob")
-//                titanicSailor.introduceMyself()
-//
-//                let titanic = Titanic(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
-//                titanic.introduceAll()
+        //                let alexSailor = Sailor(name: "Alex")
+        //                let momoSailor = Sailor(name: "Momo")
+        //                let benSailor = Sailor(name: "Ben")
+        //                let bodSailor = Sailor(name: "Bob")
+        //                let jacknSailor = Sailor(name: "Jack")
+        //
+        //
+        //                let titanicSailor = TitanicSeilor(name: "Bob")
+        //                titanicSailor.introduceMyself()
+        //
+        //                let titanic = Titanic(nameOfShip: "Black Sheap", nameOfSailors: [alexSailor.name, momoSailor.name, benSailor.name, bodSailor.name, jacknSailor.name])
+        //                titanic.introduceAll()
         // 5
-//        let calc = Calculator()
-//        calc.plus(numberOne: 1, numberTwo: 1)
-//        calc.degree(numberOne: 20, numberTwo: 313)
-//        //
-//        let calcPro = CalculatorPro()
-//        calcPro.percentageСalculation(numberOne: 100, percent: 25, degree: 2)
+        //        let calc = Calculator()
+        //        calc.plus(numberOne: 1, numberTwo: 1)
+        //        calc.degree(numberOne: 20, numberTwo: 313)
+        //        //
+        //        let calcPro = CalculatorPro()
+        //        calcPro.percentageСalculation(numberOne: 100, percent: 25, degree: 2)
         //6
-//
-//        let superCalc = SuperProCalc()
-//        superCalc.hardPercent(firstSum: 100, years: 1, yearPercent: 2)
+        //
+        //        let superCalc = SuperProCalc()
+        //        superCalc.hardPercent(firstSum: 100, years: 1, yearPercent: 2)
+        
+        
+        //
+        
+        //
+        //
+        //        let bob = King(name: "Bob")
+        //        let tod = King(name: "Tod")
+        //
+        //        let ben = Farmer(name: "Ben")
+        //        let alex = Farmer(name: "Alex")
+        //
+        //        let tamara = Inhabitant(name: "Tamara")
+        //        let irina = Inhabitant(name: "Irina")
+        //
+        //        let oleg = Butcher(name: "Oleg")
+        //        let sasha = Butcher(name: "Sasha")
+        //
+        //        let artem = Soldier(name: "Artem")
+        //        let tim = Soldier(name: "Tim")
+        //
+        //
+        //        let blackTaverna = Taverna()
+        //
+        //        blackTaverna.movers.append(ben)
+        //        blackTaverna.movers.append(alex)
+        //        blackTaverna.movers.append(tamara)
+        //        blackTaverna.movers.append(irina)
+        //        blackTaverna.fighters.append(oleg)
+        //        blackTaverna.fighters.append(sasha)
+        //
+        //        blackTaverna.enterTaverna(hero: bob)
+        //        blackTaverna.enterTaverna(hero: tod)
+        //        blackTaverna.enterTaverna(hero: artem)
+        //        blackTaverna.enterTaverna(hero: tim)
+        
 
+        
     }
 }
 
@@ -174,3 +211,181 @@ class SuperProCalc: CalculatorPro {
     }
 }
 
+//Попробуем создать персонажей для игры.
+// 7. Создайте протокол Movable, в котором будет метод run(), протокол Fightable с методом fight()
+// Создайте протокол Hero, который включает оба протокола и свойство name
+
+
+protocol Movable {
+    
+    func run()
+}
+
+protocol Fightable {
+    
+    func fight()
+}
+
+protocol Hero: Movable, Fightable {
+    
+    var name: String {get set}
+}
+
+//8. Создайте не менее двух персонажей для игры на каждый протокол из трех
+// описанных выше. Например struct Farmer: Movable { … }, struct Butcher: Fightable и т д.
+// Реализуйте методы в зависимости от структуры которую вы используете. В нашем
+// примере в реализации метода run() у структуры Farmer мы можем вывести print(“Farmer:
+// Run”), в методе fight() у Butcher можем вывести print(“Butcher: fight”)
+
+
+struct King: Hero {
+    
+    var name: String
+    
+    func run() {
+        print("King \(name): run") }
+    
+    func fight() {
+        print("King \(name): fight")
+    }
+    
+}
+
+
+struct Farmer: Movable {
+    
+    var name: String
+    
+    func run() {
+        print("Farmer \(name): run")
+    }
+}
+
+
+struct Inhabitant: Movable {
+    
+    var name: String
+    
+    func run() {
+        print("Inhabitant \(name): run")
+    }
+}
+
+
+struct Butcher: Fightable {
+    var name: String
+    
+    func fight() {
+        print("Butcher \(name): fight")
+    }
+}
+
+
+struct Soldier: Hero {
+    var name: String
+    
+    func run() {
+        print("Soldier \(name): run")
+    }
+    
+    func fight() {
+        print("Soldier \(name): fight")
+    }
+}
+
+
+//9. Создайте класс Tavern (таверна). У нее должен быть массив всех юнитов в таверне,
+//которые умеют сражаться: var fighters: [Fightable] = [], и аналогичный массив movers,
+//принимающий объекты что могут двигаться. Также в таверне будет метод enterTavern(hero:
+//Hero)
+//В реализации этого метода будет:
+// вызываться метод fight у Hero
+// выполняться цикл for по массиву movers, у каждого элемента будет вызываться метод run
+// аналогично с предыдущим пунктом, только у fighters метод fight()
+
+//10. Время протестировать все созданные типы и протоколы.
+
+//Создайте экземпляры всех типов: по 2 персонажа для Movable, Fightable и Hero. Пример: var
+//alice = Fermer() … Создайте экземпляр таверны.
+
+//Присвойте свойствам таверны значения: в массив movers передайте экземпляры что могут
+//двигаться, в массив fighters тех кто могут сражаться
+
+//Обратитесь к таверне и вызовите метод enterTavern для двух экземпляров соответствующих
+//протоколу Hero
+
+//В итоге в консоли должно пройти текстовое сражение, вывод имени героя, посетившего
+//таверну и начавшего сражение, после имена тех кто разбегаются, после имена тех кто
+//сражается
+
+
+
+class Taverna {
+    
+    var fighters: [Fightable] = []
+    var movers: [Movable] = []
+    
+    func enterTaverna(hero: Hero) {
+        hero.fight()
+        
+        for item in movers {
+            item.run()
+        }
+        for item in fighters {
+            item.fight()
+        }
+    }
+}
+
+
+//11*. Лучший способ закрепить информацию, придумать свой способ применения протоколов.
+// Необходимо создать свою мини игру, где вы могли бы использовать протоколы, но будет минимальный набор требований:
+// Должны быть функции, возвращающие опциональные значения
+// Эти функции должны вызываться и опционалы должны быть “развернуты”
+
+
+
+
+
+
+
+//12. Напишите расширения на следующие типы:
+// Int - Функция возводит число в N-ую степень. Попробуйте решить задачу без использования
+// стандартной функции pow(), решите ее с использованием циклов
+// String - Вычисляемое свойство isPalindrome. Возвращает Bool, в зависимости от того является
+//ли строка палиндромом
+//Не забывайте, что вы можете искать информацию в интернете. Протестируйте новые расширения c
+//разными данными, используйте не менее трех тестов на каждый тип, которые максимально
+//отличаются между собой
+//13*. Вернитесь к созданной вами игре во втором задании и добавьте для всех протоколов
+//реализацию методов по умолчанию
+
+
+extension Int {
+    
+    func numberDegree(number: Int, degree: Int) -> Int {
+        var answer = 1
+        
+        for _ in 1...degree {
+            answer *= number
+        }
+    
+        return answer
+    }
+    
+}
+
+extension String {
+    
+    var isPalindrome: Bool {
+        
+        get {
+            
+            return true
+        }
+        
+    }
+     
+    
+    
+}
