8ffdd745dc692fab7b9b3877f93f2250769a34ac
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-04-09 23:04:18 +0300

    third test task

---
+++ HomeWork/HomeWork/ThirdTask/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  HomeWork
+//
+//  Created by Олег Курбатов on 09.04.2023.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+// Повторите дизайн отображенный на картинках. Реализуйте интерфейс с помощью constraints,
+//    так чтобы картинка была адаптирована под любое устройство и любую ориентацию экрана.
+    
+    
+}
