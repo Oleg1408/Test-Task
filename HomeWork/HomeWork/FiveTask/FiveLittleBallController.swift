c8b479ac061b053b7e3cf7f8106d4d6d15783690
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-05-22 22:47:38 +0300

    finish Five Task

--- HomeWork/HomeWork/FiveTask/FiveLittleBallController.swift
+++ HomeWork/HomeWork/FiveTask/FiveLittleBallController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class FiveLittleBallController: UIViewController {
-
+    
+    
+    @IBOutlet weak var settingsView: UIView!
+    @IBOutlet weak var settingsButton: UIButton!
+    
+    @IBOutlet weak var rightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        settingsOrangeView()
+        settingsCentralButton()
+        
     }
     
-
-
+    private func settingsOrangeView() {
+        settingsView.layer.cornerRadius = 50
+    }
+    
+    private func settingsCentralButton() {
+        settingsButton.setTitle("Run!", for: .normal)
+    }
+    
+    
+    @IBAction func pressButton(_ sender: Any) {
+    
+        UIView.animate(withDuration: 0, delay: 0, options: []) {
+            self.rightConstraint.constant = 16
+            self.view.layoutIfNeeded()
+        } completion: { completed in
+            UIView.animate(withDuration: 2, delay: 0, options: []) {
+                self.settingsView.frame.origin.x = 16
+                self.view.layoutIfNeeded()
+            } completion: { completed in
+                UIView.animate(withDuration: 2, delay: 0, options: []) {
+                    self.settingsView.frame.origin.y = 16
+                    self.view.layoutIfNeeded()
+                } completion: { completed in
+                    UIView.animate(withDuration: 2, delay: 0, options: []) {
+                        self.settingsView.frame.origin.x = 300
+                        self.view.layoutIfNeeded()
+                    } completion: { completed in
+                        UIView.animateKeyframes(withDuration: 2, delay: 0, options: []) {
+                            self.settingsView.frame.origin.y = 700
+                            self.view.layoutIfNeeded()
+                        } completion: { _ in
+                            self.settingsView.isHidden = true
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
+
+
