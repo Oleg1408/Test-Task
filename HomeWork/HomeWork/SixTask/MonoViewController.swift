e40d37c85c4443a8923edcdd1e6143d4147ed172
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2023-05-23 22:55:59 +0300

    first step six task

---
+++ HomeWork/HomeWork/SixTask/MonoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MonoViewController.swift
+//  HomeWork
+//
+//  Created by Олег Курбатов on 22.05.2023.
+//
+
+import UIKit
+
+class MonoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
